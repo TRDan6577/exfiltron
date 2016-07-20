982ee113543c77c10b9630be0e5cefe0ff5cea54
trd6577 <trd6577@g.rit.edu>
2016-07-20 19:13:29 -0400

    Ignoring *.swp and fixed parsing errors
    I added *.swp to the list of file names to ignore and fixed the
    errors that could occur when using argparse. I also got rid of
    the scapy warning message and the program now properly exits
    if scapy is not properly installed

--- exfiltron.py
+++ exfiltron.py
@@ -6,14 +6,18 @@ Purpose: This is the main running program for the Exfiltron framework. This
         desired commands using other parts of this framework.
 """
 
+
+import argparse     # For parsing command line arguments
+import logging      # Get rid of that pesky scapy IPv6 warning
+import sys          # If unexpected results occur, stop usage (and call your doctor)
+
 try:
+    logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
     from scapy.all import *  # Use the almighty scapy package
 except ImportError:          # Raise an error if scapy is not installed
-    print("Could not import scapy. Is it installed?")
-    SystemExit()
-
-import argparse              # For parsing command line arguments
-
+    print("Could not import scapy. Is it installed? Are you running python2?")
+    print("Exiting...")
+    sys.exit()
 
 """
 Purpose: Parses the command line args and sends them back to main for
@@ -22,45 +26,49 @@ Purpose: Parses the command line args and sends them back to main for
 @return (List) a list of parsed arguments to interpret
 """
 def setArgParserOptions():
-    parser = argparse.ArgumentParser(description='
-     ####### ##  ## ####### #### ####   ###### ######   #####  ##   ## 
-      ##   # ##  ##  ##   #  ##   ##    # ## #  ##  ## ##   ## ###  ## 
-      ## #    ####   ## #    ##   ##      ##    ##  ## ##   ## #### ## 
-      ####     ##    ####    ##   ##      ##    #####  ##   ## ## #### 
-      ## #    ####   ## #    ##   ##   #  ##    ## ##  ##   ## ##  ### 
-      ##   # ##  ##  ##      ##   ##  ##  ##    ##  ## ##   ## ##   ## 
-     ####### ##  ## ####    #### ####### ####  #### ##  #####  ##   ##
+    # Create the argument parser
+    parser = argparse.ArgumentParser(formatter_class=
+                                     argparse.RawDescriptionHelpFormatter,description=('' +
+     '####### ##  ## ####### #### ####   ###### ######   #####  ##   ##\n' +
+     ' ##   # ##  ##  ##   #  ##   ##    # ## #  ##  ## ##   ## ###  ##\n' +
+     ' ## #    ####   ## #    ##   ##      ##    ##  ## ##   ## #### ##\n' +
+     ' ####     ##    ####    ##   ##      ##    #####  ##   ## ## ####\n' +
+     ' ## #    ####   ## #    ##   ##   #  ##    ## ##  ##   ## ##  ###\n' +
+     ' ##   # ##  ##  ##      ##   ##  ##  ##    ##  ## ##   ## ##   ##\n' +
+     '####### ##  ## ####    #### ####### ####  #### ##  #####  ##   ##\n' +
 
-     Exfiltron is a data exfiltration framework designed to help you get data
-     off of a machine without tripping an IDS or alerting a systems
-     administrator.', prog='exfiltron.py')
+     '\nExfiltron is a data exfiltration framework designed to help you get' +
+     ' data off of a machine without tripping an IDS or alerting a system' +
+     ' administrator.'), prog='exfiltron.py')
 
-    parser.add_argument('exfiltrate', metavar='method', type=string,
-                        action='store_true', dest='method', help=("This is" +
-                        " the method of exfiltration. Your options are: icmp"))
+    # Add the method of exfiltration option
+    parser.add_argument('method', type=str, help=('This is the method of ' +
+                        'exfiltration. Your options are: icmp'))
 
-    parser.add_argument('-f', '--file-name', metavar='-f', type=string,
-                        action='store_true', dest='file', help=("The name of" +
-                        " the file you wish to.. *ahem*... 'borrow'"))
+    # Add the file name option
+    parser.add_argument('-f', '--file-name', type=str, help=("The name of" +
+                        " the file you wish to.. *ahem*... 'borrow'"),
+                        required=True)
 
-    parser.add_argument('-d', '-dest-ip', metavar='-d', type=string,
-                        action='store_true', dest='destIP', help=("The IP " +
-                        "address you want the data sent to"))
+    # Add the destination IP address option
+    parser.add_argument('-d', '--dest-ip', type=str, help=("The IP " +
+                        "address you want the data sent to. Please note " +
+                        "that this option is required"), required=True)
 
-    parser.add_argument('-s', '--source-IP', metavar='[-s]', type=string,
-                        action='store_true', dest='srcIP', help=("The spoofed" +
-                        " IP address you want the data sent from. By default" +
-                        ", the data is sent from the actual IP. Use this " +
-                        "with caution. This may set off an alarm if the " +
-                        "network is using DHCP snooping"))
+    # Add the source IP optional argument
+    parser.add_argument('-s', '--source-IP', type=str,
+                        help=("The spoofed IP address you want the data sent" +
+                        " from. By default, the data is sent from the actual" +
+                        "IP address of the machine you're on. Please note " +
+                        "that this option is required"))
 
-    parser.add_argument('-a', '--data-per-packet', metavar='[-a]', type=int,
-                        action='store_true', dest='amountOfData', help=("By" +
-                        " default, the amount of data stored in a packet is" +
-                        " the same as it normally is to decrease the chances" +
-                        " of detection by an IDS (example: ICMP exfiltration" +
-                        " will have a default packet size of 64 bytes). Use" +
-                        " this option to send more or less data"))
+    # Add the data per packet optional argument
+    parser.add_argument('-a', '--data-per-packet', type=int,
+                        help=("By default, the amount of data stored in a " +
+                        "packet is the same as it normally is to decrease " +
+                        "the chances of detection by an IDS (example: ICMP" +
+                        "exfiltration will have a default packet size of " +
+                        "64 bytes). Use this option to send more or less data"))
 
     return parser.parse_args()
 
@@ -68,3 +76,6 @@ def setArgParserOptions():
 def main():
     # Parse the commands from the command line with argparser
     args = setArgParserOptions()
+
+if (__name__ == '__main__'):
+    main()
